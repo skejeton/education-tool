f8398f99c5f5df8fc0ef64b69dc08064ec8fc7e4
skejeton <ishidex2@gmail.com>
2023-04-17 22:39:36 +0400

    update wasm build script

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,2 +1,2 @@
 mkdir -p ./bin/wasm-out
-em++ -Ilib -O3 --shell-file=scripts/shell.html -s ASYNCIFY=1 -sFULL_ES2 -s ALLOW_MEMORY_GROWTH=1 -lGL -lc -lm lib/imgui/imgui.cpp lib/imgui/imgui_demo.cpp lib/imgui/imgui_draw.cpp lib/imgui/imgui_tables.cpp lib/imgui/imgui_widgets.cpp src/*.cpp -o bin/wasm-out/output.html
+em++ -Ilib -O3 --shell-file=scripts/shell.html -sASYNCIFY=1 -sSTACK_SIZE=1MB -sFULL_ES2 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS="['_malloc', '_calloc', '_free', '_main']" -lGL -lc -lm lib/imgui/imgui.cpp lib/imgui/imgui_demo.cpp lib/imgui/imgui_draw.cpp lib/imgui/imgui_tables.cpp lib/imgui/imgui_widgets.cpp src/*.cpp -o bin/wasm-out/output.html
