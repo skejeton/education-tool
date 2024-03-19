678e255f8f23b03c739a14b72770b40d5db77aac
skejeton <ishidex2@gmail.com>
2024-03-19 11:41:01 -0300

    Fix: Delete type mismatch (asan)

--- catedu/sys/sokol_setup.hpp
+++ catedu/sys/sokol_setup.hpp
@@ -2,18 +2,21 @@
 #include "sokol/sokol_app.h"
 #include "sokol/sokol_log.h"
 
-#define SOKOL_SETUP_QUICK_ENTRYPOINT(Type, title)\
-    sapp_desc sokol_main(int argc, char *argv[])\
-    {\
-        (void) argc; (void) argv;\
-        return sokol_setup_default(title, new Type());\
+#define SOKOL_SETUP_QUICK_ENTRYPOINT(Type, title)                              \
+    sapp_desc sokol_main(int argc, char *argv[])                               \
+    {                                                                          \
+        (void)argc;                                                            \
+        (void)argv;                                                            \
+        return sokol_setup_default(title, new Type());                         \
     }
 
-struct SokolSetup {
+struct SokolSetup
+{
     virtual void init() = 0;
     virtual void frame() = 0;
     virtual void cleanup() = 0;
     virtual void input(const sapp_event *event) = 0;
+    virtual ~SokolSetup() = default;
 };
 
 sapp_desc sokol_setup_default(const char *title, SokolSetup *setup);
